c7d660e0b796b8315daa6b5d85895e07593daf4e
(no author) <(no author)@61386e47-0a11-0410-a82a-e2dc294cd3a6>
2008-01-21 20:44:26 +0000

    added comment
    git-svn-id: http://www.smartwerkz.com/svn/public/projects/jmork/trunk@78 61386e47-0a11-0410-a82a-e2dc294cd3a6

--- src/test/java/mork/gui/GuiTest.java
+++ src/test/java/mork/gui/GuiTest.java
@@ -8,6 +8,7 @@ public class GuiTest extends TestCase {
 		ProfileLocator locator = new ProfileLocator();
 		String result = locator.locateFirstThunderbirdAddressbookPath();
 		assertNotNull(result);
+		//TODO this test does not pass on linux; result is similar to /home/user/.mozilla-thunderbird/abc1234.default
 		assertTrue(result.contains("Thunderbird"));
 		assertTrue(result.contains("Profiles"));
 	}
