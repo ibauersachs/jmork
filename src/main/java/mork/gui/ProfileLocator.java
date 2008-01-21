d157419d7e6586071e602ccadd7231d69daa9af5
(no author) <(no author)@61386e47-0a11-0410-a82a-e2dc294cd3a6>
2008-01-21 20:41:44 +0000

    make it work on linux systems
    git-svn-id: http://www.smartwerkz.com/svn/public/projects/jmork/trunk@77 61386e47-0a11-0410-a82a-e2dc294cd3a6

--- src/main/java/mork/gui/ProfileLocator.java
+++ src/main/java/mork/gui/ProfileLocator.java
@@ -12,59 +12,85 @@ import java.util.Properties;
  * 
  * @author mhaller
  */
-class ProfileLocator {
+class ProfileLocator
+{
 
-	public String locateFirstThunderbirdAddressbookPath() {
-		// 1. Use environment variable "APPDATA"/Thunderbird/
-		String appData = System.getenv().get("APPDATA");
-		if (appData == null)
-		{
-		   return null;
-		}
-		File appDataFolder = new File(appData);
-		if (!appDataFolder.exists()) {
-			return null;
-		}
-		if (!appDataFolder.isDirectory()) {
-			return null;
-		}
-		File thunderbird = new File(appDataFolder, "Thunderbird");
-		if (!thunderbird.exists()) {
-			return null;
-		}
-		if (!thunderbird.isDirectory()) {
-			return null;
-		}
-		File profiles = new File(thunderbird, "profiles.ini");
-		if (!profiles.exists()) {
-			return null;
-		}
-		if (!profiles.isFile()) {
-			return null;
-		}
-		if (!profiles.canRead()) {
-			return null;
-		}
-		Properties props = new Properties();
-		try {
-			props.load(new FileInputStream(profiles));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		if (!props.containsKey("Path")) {
-			return null;
-		}
-		String path = (String) props.getProperty("Path");
-		File firstProfileFolder = new File(thunderbird, path);
-		if (!firstProfileFolder.exists()) {
-			return null;
-		}
-		if (!firstProfileFolder.isDirectory()) {
-			return null;
-		}
-		return firstProfileFolder.getAbsolutePath();
-	}
+   private File getThunderbirdDir()
+   {
+      // 1. Try environment variable "APPDATA"/Thunderbird/
+      String appData = System.getenv().get("APPDATA");
+      if (appData != null && new File(appData).exists())
+      {
+         File appDataFolder = new File(appData);
+         if (appDataFolder.isDirectory())
+         {
+            File thunderbird = new File(appDataFolder, "Thunderbird");
+            if (thunderbird.exists() && thunderbird.isDirectory() )
+            {
+               return thunderbird;
+            }
+         }
+      }
+      
+      // 2. Try "<user.home>/.mozilla-thunderbird
+      File userhome = new File(System.getProperty("user.home"));
+      File thunderbird = new File(userhome, ".mozilla-thunderbird");
+      if (thunderbird.exists() && thunderbird.isDirectory())
+      {
+         return thunderbird;
+      }
+      
+      return null;
+   }
+
+   public String locateFirstThunderbirdAddressbookPath()
+   {
+      File thunderbird = getThunderbirdDir();
+      if (thunderbird == null)
+      {
+         return null;
+      }
+      File profiles = new File(thunderbird, "profiles.ini");
+      if (!profiles.exists())
+      {
+         return null;
+      }
+      if (!profiles.isFile())
+      {
+         return null;
+      }
+      if (!profiles.canRead())
+      {
+         return null;
+      }
+      Properties props = new Properties();
+      try
+      {
+         props.load(new FileInputStream(profiles));
+      }
+      catch (FileNotFoundException e)
+      {
+         e.printStackTrace();
+      }
+      catch (IOException e)
+      {
+         e.printStackTrace();
+      }
+      if (!props.containsKey("Path"))
+      {
+         return null;
+      }
+      String path = (String) props.getProperty("Path");
+      File firstProfileFolder = new File(thunderbird, path);
+      if (!firstProfileFolder.exists())
+      {
+         return null;
+      }
+      if (!firstProfileFolder.isDirectory())
+      {
+         return null;
+      }
+      return firstProfileFolder.getAbsolutePath();
+   }
 
 }
